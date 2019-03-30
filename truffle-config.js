5d93c0db529fe4a14104362baa4ace6f640ee35f
tinyjin <baram991103@gmail.com>
2019-03-30 17:45:47 +0900

    테스트넷 배포

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,21 @@
-const path = require("path");
+const path = require('path');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = 'fatigue mouse minute fitness click action marine razor vivid give wolf ginger';
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-    development: {
+    ganache: {
       host: "localhost",
-      port: 7545,
+      port: 8545,
       network_id: "*"
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/cc50593d72164f60aba302a87a64b3c4'),
+      network_id: '3',
+      gas: 4500000,
+      gasPrice: 10000000000,
     }
   },
   compilers: {
